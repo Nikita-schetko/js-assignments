90ccf9ab08f3c47a318a4a6f5024367164637b75
Nikita-schetko <corrosion.by@gmail.com>
2017-10-27 23:47:03 +0300

    - Strings tasks finished; - Several tasks for numbers

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -305,7 +305,17 @@ function isString(value) {
  *   'K♠' => 51
  */
 function getCardId(value) {
-    throw new Error('Not implemented');
+    var cardArray = ['A♣', '2♣', '3♣', '4♣', '5♣', '6♣', '7♣', '8♣', '9♣', '10♣', 'J♣', 'Q♣', 'K♣',
+        'A♦', '2♦', '3♦', '4♦', '5♦', '6♦', '7♦', '8♦', '9♦', '10♦', 'J♦', 'Q♦', 'K♦',
+        'A♥', '2♥', '3♥', '4♥', '5♥', '6♥', '7♥', '8♥', '9♥', '10♥', 'J♥', 'Q♥', 'K♥',
+        'A♠', '2♠', '3♠', '4♠', '5♠', '6♠', '7♠', '8♠', '9♠', '10♠', 'J♠', 'Q♠', 'K♠'];
+
+        function isBigEnough(element) {
+            return element == value;
+        }
+
+        return cardArray.findIndex(isBigEnough);
+        
 }
 
 
